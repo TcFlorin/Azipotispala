72205b88475f61ca666ac8d7e36447d61ae92a49
TcFlorin <tcflorin2000@gmail.com>
2025-10-24 10:58:21 +0300

    arata mai bine

--- lib/main.dart
+++ lib/main.dart
@@ -24,37 +24,87 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: const Color(0XFFE3F2FD),
         appBar: AppBar(
-          title: const Text('Spal azi ?'),
-        ),
+          backgroundColor: const Color(0xFF1565C0),
+          title: const Text('Spal azi ?',
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+          ),
+          centerTitle: true,
+        ),  
         body: Center(
-          child: _showQuestion
-            ? _buildQuestion()
-            : _showAnimation
-              ? _buildAnimation()
-              : _buildDecision()
+          child: AnimatedSwitcher(
+            duration: const Duration(milliseconds: 800),
+            switchInCurve: Curves.easeIn,
+            switchOutCurve: Curves.easeOut,
+            child: _showQuestion
+              ? _buildQuestion()
+              : _showAnimation
+                ? _buildAnimation()
+                : _buildDecision()
+        ),
         ),
       );
+      
   }
 
   Widget _buildQuestion(){
     return GestureDetector(
+      key: const Key('question'),
       onTap: _startAnimation,
-      child: Image.asset('assets/images/spalmachine.png'),
+      child: _buildImageContainer('assets/images/spalmachine.png'),
     );
 
   }
 
 
   Widget _buildAnimation() {
-    return Image.asset('assets/images/Animated.gif');
+    return _buildImageContainer('assets/images/Animated.gif'
+      , key: const Key('animation')
+      );
   }
 
   Widget _buildDecision() {
     return _status
-      ? Image.asset('assets/images/yes-machine.png')
-      : Image.asset('assets/images/no-machine.png');
+      ? _buildImageContainer('assets/images/yes-machine.png',
+      key: const Key('yes')
+      )
+      : _buildImageContainer('assets/images/no-machine.png',
+      key: const Key('no')
+      );
   
+  }
+
+  Widget _buildImageContainer(String path, {Key ? key}) {
+    return SizedBox(
+      key: key,
+      width: 400,
+      height: 560,
+      child: Stack(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: Image.asset(
+              path,
+              width: 400,
+              height: 560,
+              fit: BoxFit.cover,
+              ),
+          ),
+        Container(
+          width: 400,
+          height: 560,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            border: Border.all(
+              color: const Color.fromARGB(255, 0, 0, 0),
+              width: 7,
+            ),
+          ),
+        )
+        ]
+    ),
+    );
   }
 
   void _startAnimation() {
